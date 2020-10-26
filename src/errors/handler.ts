6a08b4f120e8d62d94d31d0d0e7e25e961ed2691
Thiago Cesar <thiagohs619@gmail.com>
2020-10-26 18:25:52 -0300

    api finalizada

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -19,8 +19,6 @@ const errorHandler:ErrorRequestHandler = (error, request, response, next) => {
   }
   
   console.error(error);
-
-
   return response.status(500).json({message: 'Internal server error'});
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import express from 'express';
-import 'express-async-errors';
-
+import cors from 'cors';
 import path from 'path';
 
+import 'express-async-errors';
 import './database/connection';
-import errorHandler from './errors/handler';
 
 import routers from './routes';
+import errorHandler from './errors/handler';
 
 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routers);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
