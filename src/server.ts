e826bb646a622b3a396842cb81649ae532c6b838
Thiago Cesar <thiagohs619@gmail.com>
2020-10-26 17:52:52 -0300

    arquivo de tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import 'express-async-errors';
 import path from 'path';
 
 import './database/connection';
+import errorHandler from './errors/handler';
 
 import routers from './routes';
 
@@ -14,6 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(routers);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333, () => {
     console.log('Server start port 3333!');
