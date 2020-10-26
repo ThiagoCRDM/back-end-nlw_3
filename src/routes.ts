25a2e42c0e56001f9fa675bb750c58b38dae269a
Thiago Cesar <thiagohs619@gmail.com>
2020-10-26 16:25:33 -0300

    arquivo de configuraçao upload de imagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routers = Router();
+const upload = multer(uploadConfig);
 
 routers.get('/orphanages', OrphanagesController.index);
 routers.get('/orphanages/:id', OrphanagesController.show);
-routers.post('/orphanages', OrphanagesController.create);
+routers.post('/orphanages', upload.array('images') ,OrphanagesController.create);
 
 
 export default routers;
